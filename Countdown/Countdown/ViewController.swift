377df4ee79f3c615c6d37191cd0875187ad39584
Mike Moore <michaelhmoore1@gmail.com>
2022-01-26 16:51:11 -0700

    Initial setup

---
+++ Countdown/Countdown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Michael Moore on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
