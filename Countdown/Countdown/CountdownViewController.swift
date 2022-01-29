f330434ad76c60c5b53acd3d45177a9c1de50052
Mike Moore <michaelhmoore1@gmail.com>
2022-01-29 09:19:06 -0700

    created date picker; added isCalculated to Core Data

--- Countdown/Countdown/CountdownViewController.swift
+++ Countdown/Countdown/CountdownViewController.swift
@@ -28,23 +28,16 @@ class CountdownViewController: UIViewController {
     @IBOutlet private weak var longevitySliderNumberLabel: UILabel!
     @IBOutlet private weak var calculateResetButton: UIButton!
     
+    let datePicker = UIDatePicker()
+    
     private var isCalculated: Bool = false
-    private var birthDate: Double?
-    private var longevityScore: Int?
+    private var birthDate: Date = Date()
+    private var longevityScore: Int = 75
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        longevityScore = 75
-        birthDate = Date().timeIntervalSince1970
-        
-        calculateResetButton.layer.cornerRadius = 8
-        longevitySlider.minimumValue = 0
-        longevitySlider.maximumValue = 123
-        longevitySlider.value = Float(longevityScore!)
-        longevityNumberLabel.text = "\(longevityScore!)"
-        longevitySliderNumberLabel.text = "\(longevityScore!)"
-        lifeProgressView.progress = 0.75
-        
+        createDatePicker()
+        setupInitialView()
     }
     
     @IBAction func longevitySliderChangedValue(_ sender: Any) {
@@ -52,50 +45,60 @@ class CountdownViewController: UIViewController {
     }
     
     @IBAction func calculateResetButtonTapped(_ sender: Any) {
-        save { complete in
-            if complete {
-                isCalculated ? changeViewToCalculate() : changeViewToCountdown()
-                birthDateStackView.isHidden = isCalculated
-                sliderStackView.isHidden = isCalculated
-                countdownContainerView.isHidden = !isCalculated
-            } else {
-                print("display error alert")
+        isCalculated.toggle()
+        
+        if isCalculated {
+            save { complete in
+                if complete {
+                    changeViewToCountdown()
+                } else {
+                    print("display error alert")
+                }
             }
+        } else {
+            changeViewToCalculate()
         }
     }
     
-    func setupInitialView() {
-        isCalculated ? changeViewToCalculate() : changeViewToCountdown()
+    private func setupInitialView() {
+        isCalculated ? changeViewToCountdown() : changeViewToCalculate()
+        
+        calculateResetButton.layer.cornerRadius = 8
+        longevitySlider.minimumValue = 0
+        longevitySlider.maximumValue = 123
+        longevitySlider.value = Float(longevityScore)
+        longevityNumberLabel.text = "\(longevityScore)"
+        longevitySliderNumberLabel.text = "\(longevityScore)"
+        lifeProgressView.progress = 0.75
     }
     
-    func changeViewToCountdown() {
-        isCalculated.toggle()
-        calculateResetButton.setTitle("Reset", for: .normal)
+    private func changeViewToCountdown() {
         longevityScore = Int(longevitySlider.value)
-        longevityNumberLabel.text = "\(longevityScore!)"
+        longevityNumberLabel.text = "\(Int(longevitySlider.value))"
+        
+        calculateResetButton.setTitle("Reset", for: .normal)
+        
+        birthDateStackView.isHidden = isCalculated
+        sliderStackView.isHidden = isCalculated
+        countdownContainerView.isHidden = !isCalculated
     }
     
-    func changeViewToCalculate() {
-        isCalculated.toggle()
+    private func changeViewToCalculate() {
         calculateResetButton.setTitle("Calculate!", for: .normal)
+        
+        birthDateStackView.isHidden = isCalculated
+        sliderStackView.isHidden = isCalculated
+        countdownContainerView.isHidden = !isCalculated
     }
-    
-    func avenirFont(size: CGFloat) -> UIFont {
-        guard let avenirFont = UIFont(name: "AvenirNext-DemiBold", size: size) else {
-            return UIFont.systemFont(ofSize: size)
-        }
-        return avenirFont
-    }
-    
+}
+
+// MARK: - Core Data
+private extension CountdownViewController {
     func save(completion: (_: Bool) -> ()) {
-        guard let context = appDelegate?.persistentContainer.viewContext,
-              let birthDate = birthDate,
-              let longevityScore = longevityScore
-        else {
-            return
-        }
+        guard let context = appDelegate?.persistentContainer.viewContext else { return }
         
         let longevity = Longevity(context: context)
+        longevity.isCalculated = isCalculated
         longevity.birthDate = birthDate
         longevity.longevityScore = Int32(longevityScore)
         
@@ -107,7 +110,42 @@ class CountdownViewController: UIViewController {
             completion(false)
         }
     }
-    
-    
 }
 
+// MARK: - Helper Methods
+private extension CountdownViewController {
+    func createToolbar() -> UIToolbar {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        
+        toolbar.setItems([flexibleSpace, doneButton], animated: true)
+        
+        return toolbar
+    }
+    
+    @objc func donePressed() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        birthDateTextField.text = dateFormatter.string(from: datePicker.date)
+        birthDate = datePicker.date
+        view.endEditing(true)
+    }
+    
+    func createDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        birthDateTextField.inputView = datePicker
+        birthDateTextField.inputAccessoryView = createToolbar()
+    }
+    
+    func avenirFont(size: CGFloat) -> UIFont {
+        guard let avenirFont = UIFont(name: "AvenirNext-DemiBold", size: size) else {
+            return UIFont.systemFont(ofSize: size)
+        }
+        return avenirFont
+    }
+}
